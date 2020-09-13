f9a56d893d39a95bbd63ce950d04b77316fe4260
Intelligent Cat <mohammadaminkazmi@gmail.com>
2020-09-13 18:14:43 +0430

    Added daily song reupload limit

--- config/reupload.php
+++ config/reupload.php
@@ -4,8 +4,9 @@
 $api_key = "dc467dd431fc48eb0244b0aead929ccd"; // The soundcloud developer api key to use to reupload songs
 $reupUID = 0; // The UserID of the account.
 $reupAID = 0; // The AccountID of the account. Also known as extID in the "users" table.
+$level_reupload = 0; // any number for applying the limit, isLevelReuploadLimitDaily indicates that if this limit resets daily, 0 for unlimited and -1 to disable reuploading
 $isLevelReuploadLimitDaily = 0; // 0 for false and 1 for true
-$level_reupload = 0; // any number for applying the limit, isLevelReuploadLimitDaily indicates that if this limit resets daily, 0 for unlimited and -1 to disable reuploading 
 $song_reupload = 0; // just like level reuploading limit, but for songs
+$isSongsReuploadLimitDaily = 0; // just like daily limit for levels, but for songs
 $can_reupload_from_direct_links = 1; // 0 to disable direct link song reuploading and 1 for enabling it
 ?>
