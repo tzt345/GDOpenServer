8653447b5973c77b56559cf3c49c532ea8d3a3a7
Intelligent Cat <mohammadaminkazmi@gmail.com>
2020-09-13 18:17:12 +0430

    Update reupload.php

--- config/reupload.php
+++ config/reupload.php
@@ -7,6 +7,6 @@ $reupAID = 0; // The AccountID of the account. Also known as extID in the "users
 $level_reupload = 0; // any number for applying the limit, isLevelReuploadLimitDaily indicates that if this limit resets daily, 0 for unlimited and -1 to disable reuploading
 $isLevelReuploadLimitDaily = 0; // 0 for false and 1 for true
 $song_reupload = 0; // just like level reuploading limit, but for songs
-$isSongsReuploadLimitDaily = 0; // just like daily limit for levels, but for songs
+$isSongReuploadLimitDaily = 0; // just like daily limit for levels, but for songs
 $can_reupload_from_direct_links = 1; // 0 to disable direct link song reuploading and 1 for enabling it
 ?>
