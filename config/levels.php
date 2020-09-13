e787e52b2728b96aa66a30d6872b8a11f258a90c
Intelligent-Cat <64915620+Intelligent-Cat@users.noreply.github.com>
2020-09-13 20:48:29 +0430

    Adde whole CP share config...on phone.

--- config/levels.php
+++ config/levels.php
@@ -1,5 +1,6 @@
 <?php
 $epicCP = 2; // creator points given for getting epic
+$magicCP = 2; // this counts if magic section is manual
 $isMagicSectionManual = 0; // enables magic command and gives creator points for levels that go into that section
-$magicCP = 1; // this counts if magic section is manual
+$CPSharedWhole = 0; // this indicates if shared level creator points are added equally to each creator, or split between the creators; 0 for no and 1 for yes
 ?>
