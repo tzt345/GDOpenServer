41c11ddee515da6e52890c355f7d5c6d34480f83
Jecket <35532868+Jecket22@users.noreply.github.com>
2020-09-13 18:06:45 +0200

    $IPChecking wasn't edited so

--- config/security.php
+++ config/security.php
@@ -1,5 +1,5 @@
 <?php
 $cloudSaveEncryption = 0; // 0 = password string replacement, 1 = cloud save encryption (password dependant)
 $sessionGrants = 1; // 0 = GJP check is done every time; 1 = GJP check is done once per hour; drastically improves performance, slightly descreases security
-$accountIDasIP = 1; // 0 = replaces client's IP with his accountID; 1 = always uses client's IP
+$IPchecking = 1; // 0 = Doesn't check for IP addresses; 1 = Checks IP addresses when needed. You should NOT turn this off, unless getIP() results into 127.0.0.1.
 ?>
