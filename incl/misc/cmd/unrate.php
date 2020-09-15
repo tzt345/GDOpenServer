f33bf6737b7a52c0dbea80a5bf22aa6659e37e82
Intelligent Cat <mohammadaminkazmi@gmail.com>
2020-09-15 14:01:26 +0430

    i dont really like to write "fix" every time

--- incl/misc/cmd/unrate.php
+++ incl/misc/cmd/unrate.php
@@ -11,8 +11,8 @@ function unrate($gs, $commentarray, $uploadDate, $accountID, $levelID) {
     } else {
         $keepDiff = 0;
     }
-    $starCoins = $commentarray[2];
-    $starFeature = $commentarray[3];
+    $unverifyCoins = $commentarray[2];
+    $unfeature = $commentarray[3];
     if ($keepDiff == 1) {
         $query = $db->prepare("UPDATE levels SET starStars='0', starDemon='0', starAuto='0' WHERE levelID=:levelID");
         $query->execute([':levelID' => $levelID]);
@@ -27,31 +27,31 @@ function unrate($gs, $commentarray, $uploadDate, $accountID, $levelID) {
         $query = $db->prepare("INSERT INTO modactions (type, value, value2, value3, timestamp, account) VALUES ('1', 'na', '0', :levelID, :timestamp, :id)");
         $query->execute([':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
     }
-    if(isset($starFeatured) AND is_numeric($starFeatured)){
-        if ($starFeatured >= 1) {
-            $starFeatured = 1;
-        } elseif ($starFeatured <= 0) {
-            $starFeatured = 0;
+    if(isset($unfeature) AND is_numeric($unfeature)){
+        if ($unfeature >= 1) {
+            $unfeature = 1;
+        } elseif ($unfeature <= 0) {
+            $unfeature = 0;
         }
-        if ($starFeatured == 1) {
+        if ($unfeature == 1) {
             if ($gs->checkPermission($accountID, "commandFeature")) {
-                $query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('2', :value, :levelID, :timestamp, :id)");
-                $query->execute([':value' => $starFeatured, ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);	
+                $query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('2', '0', :levelID, :timestamp, :id)");
+                $query->execute([':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);	
                 $query = $db->prepare("UPDATE levels SET starFeatured='0' WHERE levelID=:levelID");
                 $query->execute([':levelID' => $levelID]);
             }
         }
     }
-    if (isset($starCoins) AND is_numeric($starCoins)) {
-        if ($starCoins >= 1) {
-            $starCoins = 1;
-        } elseif ($starCoins <= 0) {
-            $starCoins = 0;
+    if (isset($unverifyCoins) AND is_numeric($unverifyCoins)) {
+        if ($unverifyCoins >= 1) {
+            $unverifyCoins = 1;
+        } elseif ($unverifyCoins <= 0) {
+            $unverifyCoins = 0;
         }
-        if ($starCoins == 1) {
+        if ($unverifyCoins == 1) {
             if ($gs->checkPermission($accountID, "commandVerifycoins")) {
-                $query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('3', :value, :levelID, :timestamp, :id)");
-                $query->execute([':value' => $starCoins, ':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
+                $query = $db->prepare("INSERT INTO modactions (type, value, value3, timestamp, account) VALUES ('3', '0', :levelID, :timestamp, :id)");
+                $query->execute([':timestamp' => $uploadDate, ':id' => $accountID, ':levelID' => $levelID]);
                 $query = $db->prepare("UPDATE levels SET starCoins='0' WHERE levelID=:levelID");
                 $query->execute([':levelID' => $levelID]);
             }
