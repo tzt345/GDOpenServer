3c6070102a8181cd1e53a1258780be7b8faedd84
Intelligent Cat <mohammadaminkazmi@gmail.com>
2020-09-15 17:05:21 +0430

    say goodbye bugs

--- tools/songAdd.php
+++ tools/songAdd.php
@@ -1,20 +1,34 @@
 <?php
+error_reporting(0);
 include dirname(__FILE__)."/../incl/lib/connection.php";
 require dirname(__FILE__)."/../incl/lib/generatePass.php";
 $gp = new generatePass();
 require_once dirname(__FILE__)."/../incl/lib/exploitPatch.php";
 $ep = new exploitPatch();
 include dirname(__FILE__)."/../config/reupload.php";
-if(!empty($_POST["userName"]) AND !empty($_POST["password"]) AND !empty($_POST["songLink"])){
-	$userName = $ep->remove($_POST["userName"]);
-    $password = $ep->remove($_POST["password"]);
-    $query = $db->prepare("SELECT accountID FROM accounts WHERE userName=:userName");	
-	$query->execute([':userName' => $userName]);
-	$accountID = $query->fetchColumn();
-	if ($query->rowCount() == 0) {
-        exit("Invalid or non-existant account. <a href='songAdd.php'>Try again.</a>");
-    }
-	$pass = $gp->isValidUsrname($userName, $password);
+if(!empty($_POST["songLink"])){
+	if ($song_reupload != 0) {
+		if (!empty($_POST["userName"]) AND !empty($_POST["password"])){
+			$userName = $ep->remove($_POST["userName"]);
+			$password = $ep->remove($_POST["password"]);
+			$query = $db->prepare("SELECT accountID FROM accounts WHERE userName=:userName");	
+			$query->execute([':userName' => $userName]);
+			$accountID = $query->fetchColumn();
+			if ($query->rowCount() == 0) {
+				exit("Invalid or non-existant account. <a href='songAdd.php'>Try again.</a>");
+			}
+			$pass = $gp->isValidUsrname($userName, $password);
+		} else {
+			exit('<form action="songAdd.php" method="post">
+				Username: <input type="text" name="userName">
+				<br>Password: <input type="password" name="password">
+				<br>Link: <input type="text" name="songLink">
+				<br><input type="submit" value="Add Song">
+				</form>');
+		}
+	} else {
+		$pass = 1;
+	}
 	if ($pass == 1) {
         $song = str_replace("www.dropbox.com","dl.dropboxusercontent.com", $_POST["songLink"]);
         if (filter_var($song, FILTER_VALIDATE_URL) == true) {
@@ -31,13 +45,13 @@ if(!empty($_POST["userName"]) AND !empty($_POST["password"]) AND !empty($_POST["
                     echo "Processing Soundcloud song ".htmlspecialchars($name,ENT_QUOTES)." by ".htmlspecialchars($author,ENT_QUOTES)." with the download link ".htmlspecialchars($song,ENT_QUOTES)." <br>";
                 }else{
                     if(!$array->id){
-                        exit("This song is neither downloadable, nor streamable. <a href='songAdd.php'>Try again</a>.");
+                        exit("This song is neither downloadable, nor streamable. <a href='songAdd.php'>Try again.</a>");
                     }
                     $song = trim("https://api.soundcloud.com/tracks/".$array->id."/stream?client_id=".$api_key);
                     $name = $ep->remove($array->title);
                     $author = $array->user->username;
                     $author = preg_replace("/[^A-Za-z0-9 ]/", '', $author);
-                    echo "This song isn't downloadable, attempting to insert it anyways<br>";
+                    echo "This song isn't downloadable, attempting to insert it anyways...<br>";
                 }
             }else{
                 $song = str_replace("?dl=0","",$song);
@@ -51,7 +65,7 @@ if(!empty($_POST["userName"]) AND !empty($_POST["password"]) AND !empty($_POST["
                 $author = "Reupload";
             }
             if (!$soundcloud AND $can_reupload_from_direct_links == 0) {
-                exit("Reuploading from direct links is disabled in this GDPS. <a href='songAdd.php'>Try again</a>.");
+                exit("Reuploading from direct links is disabled in this GDPS. <a href='songAdd.php'>Try again.</a>");
             }
             $ch = curl_init($song);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
@@ -108,11 +122,11 @@ if(!empty($_POST["userName"]) AND !empty($_POST["password"]) AND !empty($_POST["
 					$query->execute([':name' => $name, ':download' => $song, ':author' => $author, ':size' => $size, ':hash' => $hash]);
 					echo "Song reuploaded: <b>".$db->lastInsertId()."</b><hr>";
 				} else {
-					echo "You have reached the maximum amount of reuploading levels to this GDPS.";
+					echo "You have reached the maximum amount of reuploading songs to this GDPS.";
 				}
             }
         }else{
-            echo "The download link isn't a valid URL";
+            echo "The download link isn't a valid URL.";
         }
 	}else{
 		echo "Incorrect password. <a href='songAdd.php'>Try again.</a>";
