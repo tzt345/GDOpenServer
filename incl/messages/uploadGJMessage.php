f404b995a9e01d1b3df6a91303f8e144212db76f
Intelligent Cat <mohammadaminkazmi@gmail.com>
2020-09-17 18:11:43 +0430

    Messages remake

--- incl/messages/uploadGJMessage.php
+++ incl/messages/uploadGJMessage.php
@@ -7,41 +7,38 @@ require_once "../lib/exploitPatch.php";
 $ep = new exploitPatch();
 require_once "../lib/mainLib.php";
 $gs = new mainLib();
-$gjp =  $ep->remove($_POST["gjp"]);
-$gameVersion =  $ep->remove($_POST["gameVersion"]);
-$binaryVersion =  $ep->remove($_POST["binaryVersion"]);
-$secret =  $ep->remove($_POST["secret"]);
-$subject =  $ep->remove($_POST["subject"]);
-$toAccountID =  $ep->number($_POST["toAccountID"]);
-$body =  $ep->remove($_POST["body"]);
-$accID =  $ep->number($_POST["accountID"]);
-$query3 = "SELECT userName FROM users WHERE extID = :accID ORDER BY userName DESC";
-$query3 = $db->prepare($query3);
-$query3->execute([':accID' => $accID]);
-$userName = $query3->fetchColumn();
-//continuing the accounts system
-$id = $ep->remove($_POST["accountID"]);
-$register = 1;
-$userID = $gs->getUserID($id);
-$uploadDate = time();
-
-$blocked = $db->query("SELECT ID FROM `blocks` WHERE person1 = $toAccountID AND person2 = $accID")->fetchAll(PDO::FETCH_COLUMN);
-$mSOnly = $db->query("SELECT mS FROM `accounts` WHERE accountID = $toAccountID AND mS > 0")->fetchAll(PDO::FETCH_COLUMN);
-$friend = $db->query("SELECT ID FROM `friendships` WHERE (person1 = $accID AND person2 = $toAccountID) || (person2 = $accID AND person1 = $toAccountID)")->fetchAll(PDO::FETCH_COLUMN);
-
-$query = $db->prepare("INSERT INTO messages (subject, body, accID, userID, userName, toAccountID, secret, timestamp)
-VALUES (:subject, :body, :accID, :userID, :userName, :toAccountID, :secret, :uploadDate)");
-
+$gjp = $ep->remove($_POST["gjp"]);
+$accountID = $ep->remove($_POST["accountID"]);
 $GJPCheck = new GJPCheck();
-$gjpresult = $GJPCheck->check($gjp,$id);
-if (!empty($mSOnly[0]) and $mSOnly[0] == 2) {
-    echo -1;
-} else {
-    if ($gjpresult == 1 and empty($blocked[0]) and (empty($mSOnly[0]) || !empty($friend[0]))) {
-        $query->execute([':subject' => $subject, ':body' => $body, ':accID' => $id, ':userID' => $userID, ':userName' => $userName, ':toAccountID' => $toAccountID, ':secret' => $secret, ':uploadDate' => $uploadDate]);
-        echo 1;
-    } else {
-        echo -1;
-    }
+$gjpresult = $GJPCheck->check($gjp, $accountID);
+if ($gjpresult != 1) {
+    exit("-1");
+}
+$secret = $ep->remove($_POST["secret"]);
+$subject = $ep->remove($_POST["subject"]);
+$toAccountID = $ep->number($_POST["toAccountID"]);
+$body = $ep->remove($_POST["body"]);
+
+$query2 = $db->prepare("SELECT count(*) FROM `blocks` WHERE person1 = :toAccountID AND person2 = :accountID LIMIT 1");
+$query3 = $db->prepare("SELECT mS FROM `accounts` WHERE accountID = :toAccountID AND mS != 0 LIMIT 1");
+$query4 = $db->prepare("SELECT count(*) FROM `friendships` WHERE (person1 = :accountID AND person2 = :toAccountID) AND (person2 = :accountID AND person1 = :toAccountID) LIMIT 1");
+$query2->execute([':toAccountID' => $toAccountID, ':accountID' => $accountID]);
+$query3->execute([':toAccountID' => $toAccountID, ':accountID' => $accountID]);
+$query4->execute([':toAccountID' => $toAccountID, ':accountID' => $accountID]);
+$blocked = $query2->fetchColumn();
+$mSOnly = $query3->fetchColumn();
+$friend = $query4->fetchColumn();
+
+if ($blocked == 1 OR $mSOnly == 2) {
+    exit("-1");
+} elseif (($friend == 1 AND $mSOnly == 1) OR $mSOnly == 0) {
+    $query = $db->prepare("SELECT userName FROM users WHERE extID = :accountID LIMIT 1");
+    $query->execute([':accountID' => $accountID]);
+    $userName = $query->fetchColumn();
+    $userID = $gs->getUserID($accountID);
+    $uploadDate = time();
+    $query = $db->prepare("INSERT INTO messages (subject, body, accID, userID, userName, toAccountID, secret, timestamp) VALUES (:subject, :body, :accID, :userID, :userName, :toAccountID, :secret, :uploadDate)");
+    $query->execute([':subject' => $subject, ':body' => $body, ':accID' => $accountID, ':userID' => $userID, ':userName' => $userName, ':toAccountID' => $toAccountID, ':secret' => $secret, ':uploadDate' => $uploadDate]);
+    echo 1;
 }
 ?>
