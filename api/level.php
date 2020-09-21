39343d0ca0caff9524c75049baade03b31b5adfd
Wyliemaster <62946885+Wyliemaster@users.noreply.github.com>
2020-09-21 12:55:35 +0100

    quick api

---
+++ api/level.php
@@ -0,0 +1,49 @@
+<?php
+class levelAPI {
+    function Select(){
+        include "../incl/lib/connection.php";
+        require_once "../incl/lib/mainLib.php";
+        require_once "../incl/lib/exploitPatch.php";
+        $ep = new exploitPatch();
+        $mainLib = new mainLib();
+        if(!empty($_GET["levelID"]))
+        {
+                   $response = $_GET["levelID"];
+        }
+        else if(!empty($_POST["levelID"]))
+        {
+                   $response = $_POST["levelID"];
+        }
+        $levelID = $ep->remove($response);
+        $levels = array();
+        $data = $db->prepare('SELECT * FROM levels WHERE levelID = :levelID');
+        $data->execute(['levelID' => $levelID]);
+        while($OutputData = $data->fetch(PDO::FETCH_ASSOC)){
+            $desc = base64_decode($OutputData['levelDesc']);
+            $levels = array(
+                'levelName' => $OutputData['levelName'],
+                'levelID' => $OutputData['levelID'],
+                'creator' => $OutputData['userName'],
+                'LevelVersion' => $OutputData['levelVersion'],
+                'levelLength' => $OutputData['levelLength'],
+                'Downloads' => $OutputData['downloads'],
+                'Likes' => $OutputData['likes'],
+                'LevelPassword' => $OutputData['password'],
+                'objects' => $OutputData['objects'],
+                'coins' => $OutputData['coins'],
+                'verifiedCoins' => $OutputData['starCoins'],
+                'songID' => $OutputData['songID'],
+                'stars' => $OutputData['starStars'],
+                'featured' => $OutputData['starFeatured'],
+                'epic' => $OutputData['starEpic'],
+                'description' => $desc
+            );
+        }
+        return json_encode($levels);
+    }
+}
+
+$API = new levelAPI;
+header('Content-Type: Application/json');
+echo $API->Select();
+?>
---
+++ api/user.php
@@ -0,0 +1,51 @@
+<?php
+class banAPI {
+    function Select(){
+        include "../incl/lib/connection.php";
+        require_once "../incl/lib/mainLib.php";
+        require_once "../incl/lib/exploitPatch.php";
+        $ep = new exploitPatch();
+        $mainLib = new mainLib();
+        if(!empty($_GET["userID"])){
+                   $response = $_GET["userID"];
+        }
+        else if(!empty($_POST["userID"])){
+                   $response = $_POST["userID"];
+        }
+        $user = array();
+        $userID = $ep->remove($response);
+        $data = $db->prepare('SELECT * FROM users WHERE userID = :userID');
+        $data->execute(['userID' => $userID]);
+        while($OutputData = $data->fetch(PDO::FETCH_ASSOC)){
+            $user = array(
+                'userName' => $OutputData['userName'],
+                'accountID' => $OutputData['extID'],
+                'userID' => $OutputData['userID'],
+                'stars' => $OutputData['stars'],
+                'demons' => $OutputData['demons'],
+                'secretCoins' => $OutputData['coins'],
+                'userCoins' => $OutputData['userCoins'],
+                'cp' => $OutputData['creatorPoints'],
+                'diamonds' => $OutputData['diamonds'],
+                'cube' => $OutputData['accIcon'], 
+                'ship' => $OutputData['accShip'],
+                'ball' => $OutputData['accBall'],
+                'ufo' => $OutputData['accBird'],
+                'wave' => $OutputData['accDart'],
+                'robot' => $OutputData['accRobot'],
+                'spider' => $OutputData['accSpider'],
+                'glow' => $OutputData['accGlow'],
+                'starLeaderboardBanned' => $OutputData['isBanned'], 
+                'creatorLeaderboardBanned' => $OutputData['isCreatorBanned'], 
+                'commentBanned' => $OutputData['isCommentBanned'], 
+                'commentBanReason' => $OutputData['commentBanReason']
+            );
+        }
+        return json_encode($user);
+    }
+}
+
+$API = new banAPI;
+header('Content-Type: Application/json');
+echo $API->Select();
+?>
