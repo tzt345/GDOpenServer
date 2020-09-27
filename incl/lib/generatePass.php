64c9a7a1c5038f271aaa477ae87639db6ac43b5b
Intelligent Cat <mohammadaminkazmi@gmail.com>
2020-09-27 13:04:04 +0330

    gg added account verification check

--- incl/lib/generatePass.php
+++ incl/lib/generatePass.php
@@ -5,19 +5,23 @@ class generatePass
 		include dirname(__FILE__)."/connection.php";
 		require_once dirname(__FILE__)."/mainLib.php";
 		$gs = new mainLib();
+		include dirname(__FILE__)."/../../config/security.php";
 		$ip = $gs->getIP();
-		$newtime = time() - (60 * 60);
+		$newtime = time() - 3600;
 		$query6 = $db->prepare("SELECT count(*) FROM actions WHERE type = 6 AND timestamp > :time AND value2 = :ip");
 		$query6->execute([':time' => $newtime, ':ip' => $ip]);
 		if($query6->fetchColumn() > 7){
 			return -1;
 		}else{
-			$query = $db->prepare("SELECT accountID, salt, password, isAdmin FROM accounts WHERE userName LIKE :userName");
+			$query = $db->prepare("SELECT accountID, salt, password, isAdmin, isVerified FROM accounts WHERE userName LIKE :userName");
 			$query->execute([':userName' => $userName]);
 			if($query->rowCount() == 0){
 				return 0;
 			}
 			$result = $query->fetch();
+			if ($accountVerification != 0 AND $result["isVerified"] == 0) {
+				return -1;
+			}
 			if(password_verify($pass, $result["password"])){
 				$modipCategory = $gs->getMaxValuePermission($result["accountID"], "modipCategory");
 				if($modipCategory > 0){ //modIPs
@@ -26,12 +30,12 @@ class generatePass
 					if ($query4->fetchColumn() > 0) {
 						$query6 = $db->prepare("UPDATE modips SET IP=:hostname, modipCategory=:modipCategory WHERE accountID=:id");
 					}else{
-						$query6 = $db->prepare("INSERT INTO modips (IP, accountID, isMod, modipCategory) VALUES (:hostname,:id,'1',:modipCategory)");
+						$query6 = $db->prepare("INSERT INTO modips (IP, accountID, isMod, modipCategory) VALUES (:hostname, :id, 1, :modipCategory)");
 					}
 					$query6->execute([':hostname' => $ip, ':id' => $result["accountID"], ':modipCategory' => $modipCategory]);
 				}
 				return 1;
-			}else{
+			} else {
 				$md5pass = md5($pass . "epithewoihewh577667675765768rhtre67hre687cvolton5gw6547h6we7h6wh");
 				CRYPT_BLOWFISH or die ('-2');
 				$Blowfish_Pre = '$2a$05$';
