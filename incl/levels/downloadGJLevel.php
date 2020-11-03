7827810cc14525d40408347f1130e56e028c1dc5
Jecket22 <hark.nothing@gmail.com>
2020-11-03 17:33:47 +0100

    Revert "uhhh small fixes so far"
    This reverts commit 0e04f6700a18c340b2f7cbf11a0d18c16120dad6.

--- incl/levels/downloadGJLevel.php
+++ incl/levels/downloadGJLevel.php
@@ -72,7 +72,6 @@ if(!is_numeric($levelID)){
 		//password xor
 		$pass = $result["password"];
 		$desc = $result["levelDesc"];
-		$xorPass = 0;
 		if($gs->checkModIPPermission("actionFreeCopy") == 1){
 			$xorPass = "1";
 		}
@@ -80,7 +79,7 @@ if(!is_numeric($levelID)){
 			if($pass != 0){
 				require "../lib/XORCipher.php";
 				$xor = new XORCipher();
-				$xorPass = base64_encode($xor->cipher($pass, 26364));
+				$xorPass = base64_encode($xor->cipher($xorPass, 26364));
 			}
 		}else{
 			$desc = $ep->remove(base64_decode($desc));
