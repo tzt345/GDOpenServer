08748ffa4ee67c1bfc559186532b22e196777a17
Jecket22 <hark.nothing@gmail.com>
2020-11-26 20:49:25 +0100

    tool? tool.

---
+++ tools/changeChestRewards.php
@@ -0,0 +1,85 @@
+<title>Change chest rewards</title>
+<?php
+// THIS IS A SUPER LAZY TOOL AND MIGHT NEED TO BE RE-WORKED ON, BUT FOR NOW IT **JUST WORKS**
+if(!empty($_POST["pwcheck"])) {
+	require "../config/connection.php";
+	if ($_POST["pwcheck"] != $password) {
+		exit("Wrong password!<br><br>");
+	}
+	// small chest
+	$smolMinOrbs = $_POST["smolMiO"] || 200;
+	$smolMaxOrbs = $_POST["smolMaO"] || 400;
+	$smolMinDiamonds = $_POST["smolMiD"] || 2;
+	$smolMaxDiamonds = $_POST["smolMaD"] || 10;
+	$smolMinShards = $_POST["smolMiS"] || 1;
+	$smolMaxShards = $_POST["smolMaS"] || 6;
+	$smolMinKeys = $_POST["smolMiK"] || 1;
+	$smolMaxKeys = $_POST["smolMaK"] || 6;
+	// big chest
+	$bigMinOrbs = $_POST["bigMiO"] || 200;
+	$bigMaxOrbs = $_POST["bigMaO"] || 400;
+	$bigMinDiamonds = $_POST["bigMiD"] || 2;
+	$bigMaxDiamonds = $_POST["bigMaD"] || 10;
+	$bigMinShards = $_POST["bigMiS"] || 1;
+	$bigMaxShards = $_POST["bigMaS"] || 6;
+	$bigMinKeys = $_POST["bigMiK"] || 1;
+	$bigMaxKeys = $_POST["bigMaK"] || 6;
+	// timings
+	$smolWait = $_POST["smolWait"] || 3600;
+	$bigWait = $_POST["bigWait"] || 14400;
+	// ono
+	$string = "";
+	$string .= '$chest1minOrbs = '.$smolMinOrbs.';'.PHP_EOL;
+	$string .= '$chest1minOrbs = '.$smolMaxOrbs.';'.PHP_EOL;
+	$string .= '$chest1minOrbs = '.$smolMinDiamonds.';'.PHP_EOL;
+	$string .= '$chest1minOrbs = '.$smolMaxDiamonds.';'.PHP_EOL;
+	$string .= '$chest1minOrbs = '.$smolMinShards.';'.PHP_EOL;
+	$string .= '$chest1minOrbs = '.$smolMaxShards.';'.PHP_EOL;
+	$string .= '$chest1minOrbs = '.$smolMinKeys.';'.PHP_EOL;
+	$string .= '$chest1minOrbs = '.$smolMaxKeys.';'.PHP_EOL;
+	$string .= '$chest2minOrbs = '.$bigMinOrbs.';'.PHP_EOL;
+	$string .= '$chest2minOrbs = '.$bigMaxOrbs.';'.PHP_EOL;
+	$string .= '$chest2minOrbs = '.$bigMinDiamonds.';'.PHP_EOL;
+	$string .= '$chest2minOrbs = '.$bigMaxDiamonds.';'.PHP_EOL;
+	$string .= '$chest2minOrbs = '.$bigMinShards.';'.PHP_EOL;
+	$string .= '$chest2minOrbs = '.$bigMaxShards.';'.PHP_EOL;
+	$string .= '$chest2minOrbs = '.$bigMinKeys.';'.PHP_EOL;
+	$string .= '$chest2minOrbs = '.$bigMaxKeys.';'.PHP_EOL;
+	$string .= '$chest1wait = '.$smolWait.';'.PHP_EOL;
+	$string .= '$chest1wait = '.$bigWait.';'.PHP_EOL;
+	try {
+		file_put_contents("../config/chests.php", $string);
+		exit ("Success!");
+	} catch (Exception $e) {
+		exit ("Couldn't change rewards...");
+	}
+	
+}
+require "../config/chests.php";
+?>
+<form action="" method="POST">
+	<!-- Don't even try to SQL-Inject, there's no SQL query involved in here! ;) -->
+	Database Password <input name="pwcheck" type="password" placeholder="Check GDPS Bot DM"/><br>
+	<b>Small Chest rewards</b><br>
+	Min. Orbs <input name="smolMiO" type="number" value="<?php echo ($chest1minOrbs); ?>" min=0 /><br>
+	Max. Orbs <input name="smolMaO" type="number" value="<?php echo ($chest1maxOrbs); ?>" min=0 /><br>
+	Min. Diamonds <input name="smolMiD" type="number" value="<?php echo ($chest1minDiamonds); ?>" min=0 /><br>
+	Max. Diamonds <input name="smolMaD" type="number" value="<?php echo ($chest1maxDiamonds); ?>" min=0 /><br>
+	Min. Shards <input name="smolMiS" type="number" value="<?php echo ($chest1minShards); ?>" min=0 max=6 /><br>
+	Max. Shards (up to 6) <input name="smolMaS" type="number" value="<?php echo ($chest1maxShards); ?>" min=0 max=6 /><br>
+	Min. Keys <input name="smolMiK" type="number" value="<?php echo ($chest1minKeys); ?>" min=0 max=6 /><br>
+	Max. Keys (up to 6) <input name="smolMaK" type="number" value="<?php echo ($chest1maxKeys); ?>" min=0 max=6 /><br>
+	<b>Big Chest rewards</b><br>
+	Min. Orbs <input name="bigMiO" type="number" value="<?php echo ($chest2minOrbs); ?>" min=0 /><br>
+	Max. Orbs <input name="bigMaO" type="number" value="<?php echo ($chest2maxOrbs); ?>" min=0 /><br>
+	Min. Diamonds <input name="bigMiD" type="number" value="<?php echo ($chest2minDiamonds); ?>" min=0 /><br>
+	Max. Diamonds <input name="bigMaD" type="number" value="<?php echo ($chest2maxDiamonds); ?>" min=0 /><br>
+	Min. Shards<input name="bigMiS" type="number" value="<?php echo ($chest2minShards); ?>" min=0 max=6 /><br>
+	Max. Shards<input name="bigMaS" type="number" value="<?php echo ($chest2maxShards); ?>" min=0 max=6 /><br>
+	Min. Keys <input name="bigMiK" type="number" value="<?php echo ($chest2minKeys); ?>" min=0 max=6 /><br>
+	Max. Keys <input name="bigMaK" type="number" value="<?php echo ($chest2maxKeys); ?>" min=0 max=6 /><br>
+	<b>Waiting duration (in seconds!)</b><br>
+	Small Chest <input name="smolWait" type="number" value="<?php echo ($chest1wait); ?>" min=0 /><br>
+	Big Chest <input name="bigWait" type="number" value="<?php echo ($chest2wait); ?>" min=0 /><br>
+	<input type="submit" value="Apply" />
+</form>
